489b57e8bb3ed978e2fcad7ef9bca511fd4a2141
DESKTOP-IKDVREU\박수환 <sksk31052@hanmail.net>
2017-05-03 22:36:21 +0900

    Added Husband Moving

---
+++ GameProject/Assets/Scripts/Husband.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Husband : MonoBehaviour
+{
+
+    public enum Unit_State
+    {
+        Find_State,
+        Get_State,
+    }
+
+    public int Speed = 12;
+    public Unit_State Current_State = Unit_State.Find_State;
+    public Transform PlayerTr;
+    // Use this for initialization
+    void Start()
+    {
+        PlayerTr = this.gameObject.GetComponent<Transform>();
+
+
+        StartCoroutine(State_Look());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            PlayerTr.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            PlayerTr.transform.Translate(-Vector3.forward * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            PlayerTr.transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            PlayerTr.transform.Translate(-Vector3.left * Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            Current_State = Unit_State.Get_State;
+            StartCoroutine(Get_Time());
+        }
+    }
+
+    IEnumerator State_Look()
+    {
+        while (true)
+        {
+            Debug.Log(Current_State);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+    IEnumerator Get_Time()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Current_State = Unit_State.Find_State;
+    }
+    public bool State_identify()
+    {
+        if (Current_State == Unit_State.Find_State)
+            return false;
+        return true;
+    }
+}
--- GameProject/Assets/Scripts/Wife.cs
+++ GameProject/Assets/Scripts/Wife.cs
@@ -1,56 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Wife : MonoBehaviour {
+//public class Wife : MonoBehaviour
+//{
 
-    private Transform wifeTr;
-    private Transform playerTr;
-    private UnityEngine.AI.NavMeshAgent nvAgent;
-    private float distance = 0;
-    private float angle = 10;
-    private Transform SearchPos;
-    private bool flag = true;
-    
-
-	// Use this for initialization
-	void Start () {
-        wifeTr = this.gameObject.GetComponent<Transform>();
-        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
-  
-        nvAgent = this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
-
-        nvAgent.destination = playerTr.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+//    private Transform wifeTr;
+//    private Transform playerTr;
+//    private UnityEngine.AI.NavMeshAgent nvAgent;
+//    private float distance = 0;
+//    private float angle = 10;
+//    private Transform SearchPos;
+//    private bool flag = true;
 
 
-        nvAgent.destination = playerTr.position;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        wifeTr = this.gameObject.GetComponent<Transform>();
+//        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+//        nvAgent = this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+//        nvAgent.destination = playerTr.position;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
 
 
-        distance = nvAgent.remainingDistance;
-
-        //Vector3 vec = playerTr.position - wifeTr.position;
+//        nvAgent.destination = playerTr.position;
 
 
-        //vec.Normalize();
+//        distance = nvAgent.remainingDistance;
+
+//        //Vector3 vec = playerTr.position - wifeTr.position;
 
 
-        if (distance < 8)
-        {
-            //vec = playerTr.position - SearchPos.position;
-            nvAgent.destination = wifeTr.position;
-            
-        }
-    }
-    //void Looking(Vector3 vec)
-    //{
-    //    Quaternion q = Quaternion.LookRotation(vec);
+//        //vec.Normalize();
 
 
-    //    nvAgent.transform.rotation = q;
-    //}
+//        if (distance < 8)
+//        {
+//            //vec = playerTr.position - SearchPos.position;
+//            nvAgent.destination = wifeTr.position;
 
-}
+//        }
+//    }
+//    //void Looking(Vector3 vec)
+//    //{
+//    //    Quaternion q = Quaternion.LookRotation(vec);
+
+
+//    //    nvAgent.transform.rotation = q;
+//    //}
+
+//}
